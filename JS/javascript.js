41a42097751a7c2cb6761b06fcbd5fbe92ab0afd
Pawel Brejnak <pawelbrejnak6@gmail.com>
2023-02-14 19:17:06 +0100

    better class name in js

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,57 +1,53 @@
 {
-	const calculateResult = (pln, currency) => {
-		const euro = 4.71
-		const USD = 4.35
-		const funt = 5.26
+	const calculateResult = (PLN, currency) => {
+		const EUR = 4.71;
+		const USD = 4.35;
+		const GBP = 5.26;
 		switch (currency) {
-			case "euro":
-				return (result = pln / euro)
+			case "EUR":
+				return (result = PLN / EUR);
 
-			case "pound":
-				return (result = pln / funt)
+			case "GBP":
+				return (result = PLN / GBP);
 
-			case "usd":
-				return (result = pln / USD)
+			case "USD":
+				return (result = PLN / USD);
 		}
-	}
+	};
 
 	const assignSign = currency => {
-		let sign
+		let sign;
 		switch (currency) {
-			case "euro":
-				return (sign = "€")
+			case "EUR":
+				return (sign = "€");
 
-			case "pound":
-				return (sign = "£")
+			case "GBP":
+				return (sign = "£");
 
-			case "usd":
-				return (sign = "$")
-		}
-	}
+			case "USD":
+				return (sign = "$");
+		};
+	};
 
-	
-	const reslutTextUpdate = (sign, result) =>{
-		const resultElement = document.querySelector(".js-result")
-		resultElement.innerText = `${result.toFixed(2)} ${sign}`
-	}
+	const resultTextUpdate = (sign, result) => {
+		const resultElement = document.querySelector(".js-result");
+		resultElement.innerText = `${result.toFixed(2)} ${sign}`;
+	};
 
 	const onFormInput = () => {
-		
-		const currencyElement = document.querySelector(".js-currency")
-		const plnElement = document.querySelector(".js-pln")
+		const currencyElement = document.querySelector(".js-currency");
+		const PLNElement = document.querySelector(".js-pln");
 
-		const pln = +plnElement.value
-		const currency = currencyElement.value
-		const result = calculateResult(pln, currency)
-		const sign = assignSign(currency)
-		reslutTextUpdate(sign, result)
-		}
-
+		const PLN = +PLNElement.value;
+		const result = calculateResult(PLN, currencyElement.value);
+		const sign = assignSign(currencyElement.value);
+		resultTextUpdate(sign, result);
+	};
 
 	const init = () => {
-		const formElement = document.querySelector(".js-form")
-		formElement.addEventListener("input", onFormInput)
-	}
+		const formElement = document.querySelector(".js-form");
+		formElement.addEventListener("input", onFormInput);
+	};
 
-	init()
-}
+	init();
+};
